ac182a7319f9850218a66b4b1fc9052fe4b5136f
Taren P <63258880+TarenP@users.noreply.github.com>
2022-07-11 01:25:02 -0700

    working on logic still

--- qcoptimizer/qcoptimizer.py
+++ qcoptimizer/qcoptimizer.py
@@ -8,9 +8,12 @@ import numpy as np
 import pandas as pd
 import csv
 from itertools import combinations
+from collections import OrderedDict
+from scipy.fftpack import diff
 
 sim = Aer.get_backend('qasm_simulator')
 typeOfGates = ['x', 'y', 'z', 'h']
+passGates = ['cx']
 
 files = [
     'gates.csv',
@@ -91,76 +94,192 @@ def GateList(qubitNum, qcDF):
 
 
 def enhance(keyDF, qubitGates, typeOfGates):
-    final = []
+    print(qubitGates)
     #each qubit
+    circuit = []
     for i in qubitGates:
-        temp = []
+        print(i)
         qubit = []
-        #each gate
-        for r in i:
-            if r in typeOfGates:
-                temp.append(r)
+        temp = []
+        for g in i:
+            if g in typeOfGates:
+                temp.append(g)
             else:
                 qubit.append(temp)
                 temp = []
-                qubit.append(r)
-            #if greater than 4 items in list, check which list split will lead to the best compute time and fewest gates
+                qubit.append(g)
         qubit.append(temp)
-        print(qubit)
+        circuit.append(qubit)
+    # print('qubit')
+    # print(circuit)
     
     #create list of all viable gate combinations that don't change the permutation of the circuit
     #each set of gates for the qubit
-    for q in qubit:
-        qString = ','.join(q)
-        gateSets = []
-        replacementTimes = []
-        targetTimes = []
-        possibilities = []
-        #each gate in set
-        combo = [com for sub in range(4) for com in combinations(q, sub + 1)]
-        #print(combo)
-        for i in range(len(combo)):
-            combo[i] = list(combo[i])
-            idxString = ','.join(combo[i])
-            if idxString in qString:
-                gateSets.append(combo[i])
-        #print(q)
-        #print(gateSets)
-        #find all replacements that correspond to elements in gateSets
-        for p in range(len(gateSets)):
-            for j in range(len(keyDF)):
-                if keyDF['Target'][j].split(',') == gateSets[p]:
-                    #if they are equal set the gate combo in final list = to replacement synonym
-                    possibilities.append(keyDF['Replacement'][j].split(','))
-        print(possibilities)
-        
-        #find the time it takes to execute each target set of gates
-        for set in gateSets:
-            qc = QuantumCircuit(1, 1) #1 quantum, 1 classical
-            for j in set:
-                getattr(qc , j)(0)
-            qc.measure(0, 0)
-            result = execute(qc, backend=Aer.get_backend('qasm_simulator'), shots = 1024).result()
-            targetTimes.append(result.time_taken)
-        print(targetTimes)
-        
-        #find the time it takes to execute each replacement set of gates
-        for set in possibilities:
-            qc = QuantumCircuit(1, 1) #1 quantum, 1 classical
-            for j in set:
-                getattr(qc , j)(0)
-            qc.measure(0, 0)
-            result = execute(qc, backend=Aer.get_backend('qasm_simulator'), shots = 1024).result()
-            replacementTimes.append(result.time_taken)
-        print(replacementTimes)
-                
-    
-        '''
-        Compare the two times and see which replacements reduce the most amount of time and gates
-        Figure out how to find the optimal balance and split the gates up into sets based on these qualities
-        '''
-    
-        # final.append(qubit)
+    print(len(circuit))
+    final = [[] * 1 for i in range(len(circuit))]
+    # print('final')
+    # print(final)
+    for q in circuit:
+        for p in q:
+            qc = p
+            qnum = circuit.index(q)
+            qString = ','.join(qc)
+            # print('qnum')
+            # print(qnum)
+            # print('qstring')
+            # print(qString)
+            # print('q')
+            # print(p)
+            while len(qc) > 0:
+                gateSets = []
+                replacementTimes = []
+                targetTimes = []
+                replacements = []
+                targets = []
+                if 'cx' not in p:
+                    print(qc)
+                    print(final)
+                    #each gate in set
+                    combo = [com for sub in range(len(typeOfGates)) for com in combinations(qc, sub + 1)]
+                    for i in range(len(combo)):
+                        combo[i] = list(combo[i])
+                        #print(combo[i])
+                        idxString = ','.join(combo[i][0])
+                        if idxString in qString:
+                            gateSets.append(combo[i])
+                    print("gatesets")
+                    print(gateSets)
+                    print(len(gateSets))
+                    for gate in gateSets:
+                        # check if the count of sweet is > 1 (repeating item)
+                        if gateSets.count(gate) > 1:
+                            # if True, remove the first occurrence of sweet
+                            gateSets.remove(gate)
+                    print(gateSets)
+                    print(len(gateSets))
+                    #find all replacements that correspond to elements in gateSets
+                    found = False
+                    for n in range(len(gateSets)):
+                        for m in range(len(keyDF)):
+                            if keyDF['Target'][m].split(',') == gateSets[n]:
+                                #if they are equal set the gate combo in final list = to replacement synonym
+                                replacements.append(keyDF['Replacement'][m].split(','))
+                                targets.append(gateSets[n])
+                                found = True
+                    if found == False:
+                        for n in range(len(gateSets)):
+                            targets.append(gateSets[n])
+                            replacements.append(gateSets[n])
+
+                    # print('targs')      
+                    # print(targets)
+                    print(replacements)
+                    if len(replacements) >1:
+                        #find the time it takes to execute each target set of gates
+                        for set in targets:
+                            qc1 = QuantumCircuit(1, 1) #1 quantum, 1 classical
+                            for j in set:
+                                for g in j:
+                                    getattr(qc1 , g)(0)
+                            qc1.measure(0, 0)
+                            result = execute(qc1, backend=Aer.get_backend('qasm_simulator'), shots = 1024).result()
+                            targetTimes.append(result.time_taken)
+                        # print('target')
+                        print(len(targets))
+                        print(len(targetTimes))
+                        
+                        #find the time it takes to execute each replacement set of gates
+                        for set in replacements:
+                            qc2 = QuantumCircuit(1, 1) #1 quantum, 1 classical
+                            for j in set:
+                                getattr(qc2 , j)(0)
+                            qc2.measure(0, 0)
+                            result = execute(qc2, backend=Aer.get_backend('qasm_simulator'), shots = 1024).result()
+                            replacementTimes.append(result.time_taken)
+                        print(len(replacements))
+                        print(len(replacementTimes))
+                                
+                    
+                        '''
+                        Compare the two times and see which replacements reduce the most amount of time and gates
+                        Figure out how to find the optimal balance and split the gates up into sets based on these qualities
+                        '''
+                        timeDifference = []
+                        for i in range(len(replacementTimes)):
+                            timeDifference.append(targetTimes[i] - replacementTimes[i])
+                        # print('time difference')
+                        # print(timeDifference)
+                        tdIdx = timeDifference.index(max(timeDifference))
+                        
+                        lendifference = []
+                        for i in range(len(replacementTimes)):
+                            lendifference.append(len(targets[i]) - len(replacements[i]))
+                        ldIdx = lendifference.index(max(lendifference))
+                        
+                        #print(timeDifference)  
+                        if tdIdx == ldIdx:
+                            # print(tdIdx)
+                            # print(replacements)
+                            final[qnum].append(replacements[tdIdx])
+                            delStr = str(targets[tdIdx])
+                        else:
+                            temp = []
+                            print(max(lendifference))
+                            for i in range(len(timeDifference)):
+                                if lendifference[i] == max(lendifference):
+                                    temp.append(timeDifference[i])
+                                    print(temp)
+                            print(replacements)
+                            print(max(temp))
+                            print(timeDifference)    
+                            print(timeDifference.index(max(temp)))
+                            print("final")
+                            print(final)
+                            print(qnum)
+                            final[qnum].append(replacements[timeDifference.index(max(temp))])
+                            print(final)
+                            delStr = str(targets[timeDifference.index(max(temp))])
+                            print(delStr)
+                        print(qc)
+                        delStr = delStr.replace(',', '')
+                        print(delStr)
+                        delStr = delStr.replace("[", '')
+                        print(delStr)
+                        delStr = delStr.replace("]", '')
+                        print(delStr)
+                        delStr = delStr.replace("'", '')
+                        print(delStr)
+                        delStr = delStr.replace(" ", '')
+                        print(delStr)
+                        qString = qString.replace(',', '')
+                        print(qString)
+                        qString = qString.replace(delStr, '')
+                        print(qString)
+                        qc = list(qString)
+                        print('qc')
+                        print(qc)
+                    else:
+                        final[qnum].append(replacements[0])
+                        delStr = str(targets[0])
+                        delStr = delStr.replace(',', '')
+                        delStr = delStr.replace("[", '')
+                        delStr = delStr.replace("]", '')
+                        delStr = delStr.replace("'", '')
+                        delStr = delStr.replace(" ", '')
+                        #print(delStr)
+                        qString = qString.replace(',', '')
+                        qString = qString.replace(delStr, '')
+                        qc = list(qString)
+                        print('qc')
+                        print(qc)
+                else:
+                    print('qc')
+                    print(qc)
+                    print(qnum)
+                    final[qnum].append(p)
+                    qc = []
+            
+        print(final)
     
     #qubit
     for n in range(len(final)):
@@ -173,13 +292,16 @@ def enhance(keyDF, qubitGates, typeOfGates):
                         final[n][i] = keyDF['Replacement'][j].split(',')
 
     #create circuit
+    print(len(final))
     qc = QuantumCircuit(len(final), len(final))
     for qubit in range(len(final)):
         for g in range(len(final[qubit])):
             for j in final[qubit][g]:
-                if len(j) > 1:
-                    getattr(qc , j[2])(int(j[0]), int(j[1]))
-                else:
+                print("final[qubit][g]")
+                print(j)
+                if j in passGates:
+                    getattr(qc , final[qubit][g][2])(int(final[qubit][g][0]), int(final[qubit][g][1]))
+                elif j in typeOfGates:
                     getattr(qc , j)(qubit)
     return qc
 
@@ -218,11 +340,31 @@ def optimize(qc):
     return qc1
 
 
-qc = QuantumCircuit(1, 1)
+qc = QuantumCircuit(2, 2)
 qc.x(0)
+qc.h(0)
 qc.h(0)
 qc.z(0)
 qc.y(0)
-qc.z(0)
-qc.z(0)
+qc.x(1)
+qc.x(0)
+qc.y(0)
+qc.cx(0, 1)
+qc.x(1)
+qc.x(1)
+qc.cx(1, 0)
+
 qc1 = optimize(qc)
+print(qc)
+
+from qiskit.visualization import plot_bloch_multivector, plot_histogram, array_to_latex
+sim = Aer.get_backend('qasm_simulator')
+qc.measure(1, 1)
+result = execute(qc, backend=sim, shots = 1024).result()
+results = result.get_counts()
+print(results)
+print(qc1)
+qc1.measure(1, 1)
+result = execute(qc1, backend=sim, shots = 1024).result()
+results = result.get_counts()
+print(results)
